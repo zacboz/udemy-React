7563f85810ba748dc978ef99db4d962d16d7115f
zacboz <zacboz@gmail.com>
2017-05-15 20:25:35 -0600

    finished first section

---
+++ notes.js
@@ -0,0 +1,13 @@
+// The Complete React Web App Developer Course - Andrew Mead
+
+// atom plugins: language-babel, atom-beautify,
+
+//command line:
+// pwd - lets you see where you are running commands from, or prints the direct file path
+
+
+React
+// each component should have only one root element
+// presentational component - don't maintain state, render to the browser
+// container component - maintain state, shouldn't do much rendering other than children
+// state can be changed, but prop isn't - can't update prop values
